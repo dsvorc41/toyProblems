9f4bc4428d7912f0ac590fb44ca04f97b1b2a977
dsvorc41 <danilosvorcan@live.com>
2017-05-15 19:51:37 -0700

    Implement 1.3

---
+++ 1-3.js
@@ -0,0 +1,19 @@
+/*
+URLify: write a method to replace all spaces in a  string with ‘%20’. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given a ‘true’ length of a string. 
+*/
+
+const URLify = function(str){
+  let result = '';
+  //remove trailing spaces
+  str = str.trim();
+  for (let i = 0; i < str.length; i ++){
+    if (str[i] === ' '){
+      result += '%20';
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+};
+console.log(URLify('Hello world  '))//Hello%20world
+console.log(URLify('Mr John Smith Hello    '))//Mr%20John%20Smith%20Hello
