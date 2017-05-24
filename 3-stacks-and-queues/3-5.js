edc2cdad191e585db644d2ae1120c9ae6282c535
Dan Svorcan <danilosvorcan@live.com>
2017-05-24 10:16:48 -0700

    Initial commit for 3.6 TODO - finish 3.5. sort method

--- 3-stacks-and-queues/3-5.js
+++ 3-stacks-and-queues/3-5.js
@@ -5,47 +5,44 @@ Write a program to sort a stack such that the smallest items are on the top. You
 
 const SortStack = function(){
   this.storage = [];
-  this.tempStorage = [];
-  this.size = 0;
 }
 
 SortStack.prototype.push = function(value){
-  this.storage[this.size++] = value;
+  this.storage.push(value);
 }
 
 SortStack.prototype.pop = function(){
-  this.size && this.size--;
-  let lastValue = this.storage[this.size];
-  this.storage.length --;
-  return lastValue;
+  return this.storage.pop();
 }
 
 SortStack.prototype.peek = function(stackToCheck){
-  return this[stackToCheck][0];
+  stackToCheck = stackToCheck ? stackToCheck : this.storage;
+  return stackToCheck[0];
 }
 
 SortStack.prototype.isEmpty = function(stackToCheck){
-  return this[stackToCheck].length > 0;
+  stackToCheck = stackToCheck ? stackToCheck : this.storage;
+  return stackToCheck.length <= 0;
 }
 
 SortStack.prototype.sort = function(){
   
+
 }
 
 SortStack.prototype.length = function(){
-  return this.size;
+  return this.storage.length;
 }
 
 let testStack = new SortStack();
-testStack.push(1);
+testStack.push(4);
 testStack.push(2);
 testStack.push(3);
-testStack.push(4);
-testStack.push(5);
-testStack.push(6);
-testStack.push(7);
+testStack.push(1);
+testStack.push(10);
 testStack.push(8);
+// testStack.pop();
+testStack.sort();
 
-console.log(testStack.pop() === 8);
-console.log(testStack.length() === 7);
+// console.log(testStack.length() === 7);
 console.log(testStack);
